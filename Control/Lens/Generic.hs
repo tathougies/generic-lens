0701295f86844a337d5b4a7e75b03d27af27c8c7
Travis Athougies <travis@infer.com>
2015-02-13 16:43:37 -0800

    Added documentation

--- Control/Lens/Generic.hs
+++ Control/Lens/Generic.hs
@@ -1,4 +1,36 @@
 {-# LANGUAGE DeriveGeneric, TypeFamilies, TypeOperators, MultiParamTypeClasses, FlexibleInstances, ScopedTypeVariables, FlexibleContexts, RankNTypes #-}
+-- | Build van Laarhoven lenses from Generic data structures
+--
+-- = Example Usage (in GHCi)
+--
+-- > > :set -XDeriveGeneric -XStandaloneDeriving -XFlexibleContexts -XFlexibleInstances
+-- > > import Control.Lens.Generic
+-- > > import Control.Monad.Identity
+-- > > import GHC.Generics
+-- > > import Control.Lens
+-- > > data Employee f = Employee (f String) (f Int) deriving Generic
+-- > > let Employee (LensFor name) (LensFor age) = lenses
+--
+--   Now you can do things like
+--
+-- > > let x = Employee (return "Frank") (return 2) :: Employee Identity
+-- > > runIdentity (x ^. name)
+-- > "Frank"
+-- > > let y = x & name .~ return "Bob"
+-- > > runIdentity (y ^. name)
+-- > "Bob"
+--
+--   You can also define the lenses at the top of your module as expected
+--
+-- > Employee (LensFor name) (LensFor age) = lenses
+--
+--   The generated lenses have the correct polymorphic van Laarhoven type:
+--
+-- > > :type name
+-- > name
+-- >  :: Functor f =>
+-- >     (t String -> f (t String)) -> Employee t -> f (Employee t)
+
 module Control.Lens.Generic
     ( lenses, LensFor(..) ) where
 
